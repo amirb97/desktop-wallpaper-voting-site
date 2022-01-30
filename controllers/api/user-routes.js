c18ac4f4d4e6b039e9cc9faf019f5607a3599730
Era <amirb2467@gmail.com>
2022-01-29 22:19:40 -0800

    added get routes for all users and single user

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Wallpaper } = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
@@ -11,6 +11,33 @@ router.get('/', (req, res) => {
           console.log(err);
           res.status(500).json(err);
       });
+});
+
+// gets a single user's info
+router.get('/:id', (req, res) => {
+    User.findOne({
+        attributes: { exclude: ['password'] },
+        where: {
+            id: req.params.id
+        },
+        include:  [
+            {
+                model: Wallpaper,
+                attributes: ['id', 'title', 'wallpaper_url', 'user_id', 'elo_score']
+            }
+        ]
+    })
+      .then(dbUserData => {
+          if (!dbUserData) {
+              res.status(404).json({ message: 'No user found with this id' });
+              return;
+          }
+          res.json(dbUserData);
+      })
+      .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+      })
 });
 
 module.exports = router;
