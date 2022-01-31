ef2f4600b6a1f643b157c243e52a3297b43804da
Alan Lin <alin6120404@gmail.com>
2022-01-30 23:50:53 -0800

    initial setup

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
+const path = require('path');
 const express = require('express');
 const controllers = require('./controllers');
 const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(controllers);
 
