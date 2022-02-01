c2d854407e905a57a5b0cfcc8039360237b31a79
Era <amirb2467@gmail.com>
2022-01-31 22:24:05 -0800

    DELETE route created for users

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -73,4 +73,24 @@ router.post('/login', (req, res) => {
       });
 });
 
+// deletes a selected user
+router.delete('/:id', (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+      .then(dbUserData => {
+          if (!dbUserData) {
+              res.status(404).json({ message: 'No user found with this id' });
+              return;
+          }
+          res.json(dbUserData);
+      })
+      .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+      });
+});
+
 module.exports = router;
