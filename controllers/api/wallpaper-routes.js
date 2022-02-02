e2fe683232334c2a8df92d4cb21e7916220eccc6
bonobo <arburpee@gmail.com>
2022-02-01 19:25:59 -0800

    added delete wallpaper route

--- controllers/api/wallpaper-routes.js
+++ controllers/api/wallpaper-routes.js
@@ -1,11 +1,17 @@
 const router = require('express').Router();
-const { Wallpaper } = require('../../models');
+const { Wallpaper, User } = require('../../models');
 
 // GET /api/wallpapers
 
 router.get('/', (req, res) => {
     Wallpaper.findAll({
-        order: [['elo_score', 'DESC']]
+        order: [['elo_score', 'DESC']],
+        // include: [
+        //     {
+        //         model: User,
+        //         attributes: ['username']
+        //     }
+        // ]
     })
     .then(dbWallpaperData => res.json(dbWallpaperData))
     .catch(err => {
@@ -20,7 +26,13 @@ router.get('/:id', (req,res) => {
     Wallpaper.findOne({
         where: {
             id: req.params.id
-        }
+        },
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
     })
     .then(dbWallpaperData => {
         if (!dbWallpaperData) {
@@ -44,6 +56,26 @@ router.post('/', (req, res) => {
         elo_score: req.body.elo_score
     })
     .then(dbWallpaperData => res.json(dbWallpaperData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// DELETE /api/wallpapers/1
+router.delete('/:id', (req, res) => {
+    Wallpaper.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbWallpaperData => {
+        if (!dbWallpaperData) {
+            res.status(404).json({ message: 'no wallpaper found with this id'});
+            return;
+        }
+        res.json(dbWallpaperData);
+    })
     .catch(err => {
         console.log(err);
         res.status(500).json(err);
