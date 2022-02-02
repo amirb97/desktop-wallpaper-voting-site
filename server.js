3b084dcbd7a14383eea9614f7c8938dd153e6915
Era <amirb2467@gmail.com>
2022-02-02 03:57:19 -0800

    updated user-routes, server.js, and login/logout for better functionality

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
+const path = require('path');
 const express = require('express');
 const controllers = require('./controllers');
 const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(controllers);
 
